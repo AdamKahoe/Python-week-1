838e08d031ff3289f617c1b7eae8c41773a7e2af
AdamKahoe <89557570+AdamKahoe@users.noreply.github.com>
2021-11-16 23:13:44 -0800

    Add files via upload

---
+++ For Basic Loop 1.py
@@ -0,0 +1,19 @@
+for i in range(0,151):
+    print(i)
+
+print(list(range(5,1000,5)))
+
+for x in range(1, 101):
+    if x % 2 == 0:
+        print('Coding')
+    if x % 10 == 0:
+        print(' Dojo')
+
+sum = 0
+for x in range(0, 10):
+    if x % 2 != 0:
+        sum += x
+
+for x in range(2018, 0, -4):
+    if x > 0:
+        print(x)
